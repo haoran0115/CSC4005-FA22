12ebea69652e0596e96dd7ce34dda1ed79f60960
haoran0115 <sunhaorang160522@gmail.com>
2022-10-28 21:09:17 +0800

    hw02 report update

---
+++ hw02/scripts/demo.pthread_ds.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# build
+./scripts/build.sh 
+# pthread
+./build/bin/main.pthread_ds -nt 4
--- hw02/src/utils.h
+++ hw02/src/utils.h
@@ -13,7 +13,7 @@
 
 char *map_glut;
 int xDIM_glut, yDIM_glut;
-int width = 1000;
+int width = 500;
 int xwidth, ywidth;
 #endif
 
